8fb6a7e55bca4ddac36be1741dcaf6c6f1278b30
easonLiang <liangyx25@163.com>
2023-04-07 01:17:16 +0800

    kdl_parser/

---
+++ kdl_parser/include/kdl_parser/kdl/config.h
@@ -0,0 +1,10 @@
+#ifndef __CONFIG_H
+#define __CONFIG_H
+
+	#define KDL_INLINE 1
+	#define KDL_FRAME_WIDTH 12
+	#define KDL_INDEX_CHECK 1
+	#define KDL_USE_EQUAL 1
+	#define KDL_VERSION_STRING "eason-kdl-20230313pm"
+
+#endif //end-of-macro :: __CONFIG_H
